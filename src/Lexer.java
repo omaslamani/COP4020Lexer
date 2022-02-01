954da5676ff08d2dec1280d8233024b5bc59422b
oliviafeller <ofeller@ufl.edu>
2022-02-01 14:12:24 -0500

    testing

--- src/Lexer.java
+++ src/Lexer.java
@@ -5,7 +5,7 @@ import edu.ufl.cise.plc.LexicalException;
 public class Lexer implements ILexer {
     @Override
     public IToken next() throws LexicalException {
-        System.out.println("Testing the stuff working or nawh??");
+        System.out.println("Testing the stuff working or nawh?? test");
         return null;
     }
 
