308776da02649fb1bc746f392d79c7c80c37d9bf
omar@learndialogue.org <omaslamani@ufl.edu>
2022-02-01 13:31:25 -0500

    initial setup commit

---
+++ src/Lexer.java
@@ -0,0 +1,15 @@
+import edu.ufl.cise.plc.ILexer;
+import edu.ufl.cise.plc.IToken;
+import edu.ufl.cise.plc.LexicalException;
+
+public class Lexer implements ILexer {
+    @Override
+    public IToken next() throws LexicalException {
+        return null;
+    }
+
+    @Override
+    public IToken peek() throws LexicalException {
+        return null;
+    }
+}
