a3f3c21943cd6d99376c81d919f0f1ebe1fc3eff
oliviafeller <ofeller@ufl.edu>
2022-02-02 12:14:58 -0500

    Added to token class

--- src/Token.java
+++ src/Token.java
@@ -2,39 +2,49 @@ import edu.ufl.cise.plc.IToken;
 
 public class Token implements IToken {
 
+    private Kind kind;
+    private String rawText;
+    private SourceLocation sourceLocation;
+    private int intValue = 0;
+    private float floatValue = 0;
+    private boolean booleanValue;
+    private String stringValue;
+
+    public Token(Kind kind, String rawText){
+        this.kind = kind;
+        this.rawText = rawText;
+    }
+
+
 
     @Override
-    public Kind getKind() {
-        return null;
-    }
+    public Kind getKind() {return kind; }
 
     @Override
     public String getText() {
-        return null;
+        return rawText;
     }
 
     @Override
     public SourceLocation getSourceLocation() {
-        return null;
+        return sourceLocation;
     }
 
     @Override
-    public int getIntValue() {
-        return 0;
-    }
+    public int getIntValue() { return intValue; }
 
     @Override
     public float getFloatValue() {
-        return 0;
+        return floatValue;
     }
 
     @Override
     public boolean getBooleanValue() {
-        return false;
+        return booleanValue;
     }
 
     @Override
     public String getStringValue() {
-        return null;
+        return stringValue;
     }
 }
